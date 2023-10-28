d46986b9c2ed57715dea4d5f447a9097c23755d7
Phil Malone <phil.malone@mr-phil.com>
2023-10-28 15:37:14 -0400

    Reduced dpad speeds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GFORCE_Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GFORCE_Teleop.java
@@ -65,6 +65,16 @@ public class GFORCE_Teleop extends LinearOpMode
             double strafe = -gamepad1.left_stick_x * SAFE_STRAFE_SPEED;
             double yaw    = -gamepad1.right_stick_x * SAFE_YAW_SPEED;
 
+            if (gamepad1.dpad_left) {
+                strafe = SAFE_DRIVE_SPEED / 2.0;
+            } else if (gamepad1.dpad_right) {
+                strafe = -SAFE_DRIVE_SPEED / 2.0;
+            } else if (gamepad1.dpad_up) {
+                drive = SAFE_DRIVE_SPEED / 2.0;
+            } else if (gamepad1.dpad_down) {
+                drive = -SAFE_STRAFE_SPEED / 2.0;
+            }
+
             // Is the driver turning the robot, or should it hold its heading?
             if (Math.abs(yaw) > 0.05) {
                 // driver is commanding robot to turn, so turn off auto heading.
