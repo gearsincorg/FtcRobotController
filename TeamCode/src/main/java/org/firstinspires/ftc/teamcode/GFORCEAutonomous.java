86af6494f212a93b79e9a257ca2605bcdfaef958
Phil Malone <phil.malone@mr-phil.com>
2024-11-28 00:13:23 -0500

    Moved trajectory building into init phase.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GFORCEAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GFORCEAutonomous.java
@@ -7,19 +7,17 @@
 package org.firstinspires.ftc.teamcode;
 
 import androidx.annotation.NonNull;
-import androidx.core.math.MathUtils;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
-import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name="GFORCE Autonomous", group = "AA")
 public class GFORCEAutonomous extends LinearOpMode
@@ -30,7 +28,8 @@ public class GFORCEAutonomous extends LinearOpMode
     IntakeSubsystem intake   = new IntakeSubsystem(this);
     VisionSubsystem blob     = new VisionSubsystem(this);
 
-        @Override public void runOpMode()
+    @Override
+    public void runOpMode()
     {
         robot = new MecanumDrive(hardwareMap, new Pose2d(4, -63, Math.toRadians(90)));
         octoQuad.initialize(true);
@@ -41,46 +40,73 @@ public class GFORCEAutonomous extends LinearOpMode
         telemetry.setMsTransmissionInterval(25);
 
         //build trajectories
-        TrajectoryActionBuilder wallToSub = robot.actionBuilder(new Pose2d(4, -63, Math.toRadians(90)))
-                .lineToY(-30);
+        Action wallToSubPath = robot.actionBuilder(new Pose2d(4, -63, Math.toRadians(90)))
+                .lineToY(-30)
+                .build()
+                ;
 
-        TrajectoryActionBuilder subToAllSamples = robot.actionBuilder(new Pose2d(4, -30, Math.toRadians(90)))
+        Action subToAllSamplesPath = robot.actionBuilder(new Pose2d(4, -30, Math.toRadians(90)))
                 .setTangent(Math.toRadians(-90))
                 .splineToConstantHeading(new Vector2d(36, -24), Math.toRadians(90))
                 .splineToConstantHeading(new Vector2d(40, -12), Math.toRadians(0))
                 .splineToConstantHeading(new Vector2d(44, -24), Math.toRadians(-90))
-                .splineToConstantHeading(new Vector2d(44, -47), Math.toRadians(90))
+                .splineToConstantHeading(new Vector2d(44, -45), Math.toRadians(90))
 
                 .splineToConstantHeading(new Vector2d(44, -24), Math.toRadians(90))
                 .splineToConstantHeading(new Vector2d(48, -12), Math.toRadians(0))
                 .splineToConstantHeading(new Vector2d(53, -24), Math.toRadians(-90))
-                .splineToConstantHeading(new Vector2d(53, -47), Math.toRadians(90))
-
-                //.splineToConstantHeading(new Vector2d(53, -24), Math.toRadians(90))
-                //.splineToConstantHeading(new Vector2d(58, -12), Math.toRadians(0))
-                //.splineToConstantHeading(new Vector2d(62, -24), Math.toRadians(-90))
-                //.splineToConstantHeading(new Vector2d(62, -45), Math.toRadians(90))
+                .splineToConstantHeading(new Vector2d(53, -50), Math.toRadians(-90))
+                .build()
                 ;
 
-        TrajectoryActionBuilder samplesToSpecimen = robot.actionBuilder(new Pose2d(53, -47, Math.toRadians(90)))
+        Action samplesToSpecimenPath = robot.actionBuilder(new Pose2d(53, -50, Math.toRadians(90)))
                 .setTangent(Math.toRadians(180))
                 .splineToConstantHeading(new Vector2d(30, -52), Math.toRadians(-180))
+                .build()
                 ;
 
-        TrajectoryActionBuilder specimenToSub2 = robot.actionBuilder(new Pose2d(30, -63, Math.toRadians(90)))
+        Action specimenToSub2Path = robot.actionBuilder(new Pose2d(30, -63, Math.toRadians(90)))
                 .splineToConstantHeading(new Vector2d(12, -48), Math.toRadians(180))
                 .splineToConstantHeading(new Vector2d(0, -30), Math.toRadians(90))
+                .build()
                 ;
 
-        TrajectoryActionBuilder specimenToSub3 = robot.actionBuilder(new Pose2d(30, -63, Math.toRadians(90)))
+        Action specimenToSub3Path = robot.actionBuilder(new Pose2d(30, -63, Math.toRadians(90)))
                 .splineToConstantHeading(new Vector2d(12, -48), Math.toRadians(180))
                 .splineToConstantHeading(new Vector2d(-4, -30), Math.toRadians(90))
+                .build()
                 ;
 
-        TrajectoryActionBuilder subToSpecimen = robot.actionBuilder(new Pose2d(0, -30, Math.toRadians(90)))
+        Action subToSpecimenPath = robot.actionBuilder(new Pose2d(0, -30, Math.toRadians(90)))
                 .setTangent(Math.toRadians(-45))
                 .splineToConstantHeading(new Vector2d(30, -52), Math.toRadians(0))
+                .build()
                 ;
+
+        Action fourSpecimenAuto = new SequentialAction(
+                // Score Specimen 1 then sweep 2 more
+                arm.actionSetState(ArmStates.GRABBED),
+                wallToSubPath,
+                arm.actionClipIt(),
+                arm.actionWaitForState(ArmStates.LOWERING),
+                subToAllSamplesPath,
+                samplesToSpecimenPath,
+                // Pickup and score Specimen 2
+                actionDriveToSpecimen(),
+                arm.actionSetState(ArmStates.GRABBING),
+                arm.actionWaitForState(ArmStates.GRABBED),
+                specimenToSub2Path,
+                arm.actionClipIt(),
+                arm.actionWaitForState(ArmStates.LOWERING),
+                subToSpecimenPath,
+                // Pickup and score Specimen 3
+                actionDriveToSpecimen(),
+                arm.actionSetState(ArmStates.GRABBING),
+                arm.actionWaitForState(ArmStates.GRABBED),
+                specimenToSub3Path,
+                arm.actionClipIt(),
+                arm.actionWaitForState(ArmStates.LOWERING)
+        );
 
         // Wait for driver to press start
         telemetry.addData(">", "Touch Play to run Auto");
@@ -91,33 +117,10 @@ public class GFORCEAutonomous extends LinearOpMode
         // Run Auto if stop was not pressed.
         if (opModeIsActive())
         {
-            // Score on submersible and then push three samples into obs zone
             Actions.runBlocking(
-                    new ParallelAction(
-                            arm.actionUpdate(),
-                            new SequentialAction(
-                                    arm.actionSetState(ArmStates.GRABBED),
-                                    wallToSub.build(),
-                                    arm.actionClipIt(),
-                                    arm.actionWaitForState(ArmStates.LOWERING),
-                                    subToAllSamples.build(),
-                                    samplesToSpecimen.build(),
-                                    actionDriveToSpecimen(),
-                                    arm.actionSetState(ArmStates.GRABBING),
-                                    arm.actionWaitForState(ArmStates.GRABBED),
-                                    specimenToSub2.build(),
-                                    arm.actionClipIt(),
-                                    arm.actionWaitForState(ArmStates.LOWERING),
-                                    subToSpecimen.build(),
-                                    actionDriveToSpecimen(),
-                                    arm.actionSetState(ArmStates.GRABBING),
-                                    arm.actionWaitForState(ArmStates.GRABBED),
-                                    specimenToSub3.build(),
-                                    arm.actionClipIt(),
-                                    arm.actionWaitForState(ArmStates.LOWERING)
-
-                            )
-                    )
+                new ParallelAction(
+                    arm.actionUpdate(), fourSpecimenAuto
+                )
             );
         }
     }
@@ -125,15 +128,14 @@ public class GFORCEAutonomous extends LinearOpMode
     final double WITHIN_RANGE   = 2.00 ;
     final double APPROACH_SPEED = -0.3 ;
     final double CLICK_ON_SPEED = -0.2 ;
-
-    final double STRAFE_GAIN = 0.7 ;
-    final double MAX_TURN_POWER = 0.4 ;
-    final double YAW_GAIN_RAD = 0.02 * 180 / Math.PI;    // Strength of Yaw position control
+    final double STRAFE_GAIN    = 0.7 ;
 
     public Action actionDriveToSpecimen(){
         return new Action() {
             @Override
             public boolean run(@NonNull TelemetryPacket packet){
+
+                boolean keepGoing = true;
                 octoQuad.update();
                 double yError = octoQuad.getBackRangeInches();
                 robot.updatePoseEstimate();
@@ -158,25 +160,17 @@ public class GFORCEAutonomous extends LinearOpMode
                         drive = CLICK_ON_SPEED;
                     }
 
-                    // try to stay square to wall.
-                    double yawError = (Math.PI / 2) - robot.pose.heading.toDouble() ;
-                    double yaw = MathUtils.clamp(yawError * YAW_GAIN_RAD  , -MAX_TURN_POWER, MAX_TURN_POWER);
-
-                    telemetry.update();
-
                     // send drive power to wheels, and continue action
                     robot.setDrivePowers(new PoseVelocity2d(new Vector2d(drive, strafe), 0));
-                    return true;
                 } else {
-
-                    telemetry.update();
-
                     // stop moving and exit action.
                     robot.setDrivePowers(new PoseVelocity2d(new Vector2d(0,0), 0));
-                    return false;
+                    keepGoing = false;
                 }
+
+                telemetry.update();
+                return keepGoing;
             }
         };
     }
-
 }
